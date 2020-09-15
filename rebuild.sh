8ea5dcab10d9341ac454d08b5c08813e39cc6b61
Denis Kamotsky <dkamotsky@gmail.com>
2020-09-15 16:48:07 -0600

    Full code for building Kairos stacks.

--- rebuild.sh
+++ rebuild.sh
@@ -19,3 +19,14 @@ tag="py37-spark300-hadoop27-hive12-jdk8"
 docker build --build-arg BASE_CONTAINER=dkamotsky/scipy-notebook:py37 --tag dkamotsky/pyspark-notebook:${tag} --no-cache pyspark-notebook
 docker build --build-arg BASE_CONTAINER=dkamotsky/pyspark-notebook:${tag} --tag dkamotsky/all-spark-notebook:${tag} --no-cache all-spark-notebook
 docker push dkamotsky/all-spark-notebook:${tag}
+
+tag="py36-spark246-hadoop27-jdk8"
+docker build \
+--build-arg BASE_CONTAINER=dkamotsky/scipy-notebook:py36 \
+--build-arg spark_version=2.4.6 \
+--build-arg hadoop_version=2.7 \
+--build-arg py4j_version="0.10.7" \
+--build-arg spark_checksum="3A9F401EDA9B5749CDAFD246B1D14219229C26387017791C345A23A65782FB8B25A302BF4AC1ED7C16A1FE83108E94E55DAD9639A51C751D81C8C0534A4A9641" \
+--tag dkamotsky/pyspark-notebook:${tag} --no-cache pyspark-notebook
+docker build --build-arg BASE_CONTAINER=dkamotsky/pyspark-notebook:${tag} --tag dkamotsky/all-spark-notebook:${tag} --no-cache all-spark-notebook
+docker push dkamotsky/all-spark-notebook:${tag}
